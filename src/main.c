522b159cd14b32df68fde9d141674e8560d05f5b
Leonard <sleonar@student.ethz.ch>
2025-02-11 10:26:59 +0100

    updated

--- src/main.c
+++ src/main.c
@@ -5,11 +5,12 @@
 int main() {
     printf("Listing active network interfaces:\n");
     list_active_interfaces(); 
-
+    
     const char *interface_ip = "192.168.178.25"; 
 
     printf("Starting socket server on %s...\n", interface_ip);
     start_socket_server(interface_ip); 
 
     return 0; 
-}
+}   
+
